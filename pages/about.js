a2a6288c8eaaca3242b4b710e2dbbac638cbe7cd
markgmz92 <markgmz92@gmail.com>
2022-10-10 18:34:59 +0800

    Included head component in files

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,11 @@
-import NavBar from '../components/NavBar';
+import Head from 'next/head';
 
 function AboutPage() {
   return (
     <>
+      <Head>
+        <title>About - My Blog</title>
+      </Head>
       <main>
         <h1>About</h1>
       </main>
